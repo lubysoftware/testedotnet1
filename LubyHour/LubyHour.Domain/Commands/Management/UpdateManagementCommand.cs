b70bf0db04873ffecdb608cd3c3bd85c4424fd6e
vitim <vitim@DESKTOP-8QPURBN>
2020-08-20 21:37:38 -0300

    Atualização Management

--- LubyHour/LubyHour.Domain/Commands/Management/UpdateManagementCommand.cs
+++ LubyHour/LubyHour.Domain/Commands/Management/UpdateManagementCommand.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Flunt.Notifications;
+using Flunt.Validations;
+using LubyHour.Domain.Interfaces;
+using System;
 
 namespace LubyHour.Domain.Commands.Management
 {
-    class UpdateManagementCommand
+    public class UpdateManagementCommand : Notifiable, ICommand
     {
+        public Guid Id { get; set; }
+        public string Developer { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public void Validate()
+        {
+            AddNotifications(
+                new Contract()
+                  .Requires()
+                  .HasMinLen(Developer, 3, "Developer", "Nome do desenvolvedor deve conter no mínimo 3 caracteres!")
+                  .HasMaxLen(Developer, 100, "Developer", "Nome do desenvolvedor deve conter no máximo 100 caracteres!")
+                  .IsNotNull(Id, "Id", "Id não pode ser nulo!")
+                  .IsNotNull(StartTime, "StartTime", "Data de início não pode ser nula!")
+                  .IsNotNull(EndTime, "EndTime", "Data de finalização não pode ser nula!")
+
+            );
+        }
     }
 }
