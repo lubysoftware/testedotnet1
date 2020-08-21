53ab98500031194c08fe1a9ccd59367afc01ef1a
vitim <vitim@DESKTOP-8QPURBN>
2020-08-21 17:38:28 -0300

    Implementado a adição dos comentários no Swagger

--- LubyHour/LubyHour.Api/Startup.cs
+++ LubyHour/LubyHour.Api/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using LubyHour.Domain.Handlers;
 using LubyHour.Domain.Interfaces;
@@ -43,6 +45,9 @@ namespace LubyHour.Api
                     Title = "LubyHour",
                     Version = "v1"
                 });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                x.IncludeXmlComments(xmlPath);
             });
         }
 
@@ -63,6 +68,7 @@ namespace LubyHour.Api
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "LubyHour - V1");
+
             });
 
             app.UseEndpoints(endpoints =>
