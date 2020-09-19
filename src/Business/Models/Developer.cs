1cb468e3a1dc4dce493003f9ec8191a1b686a78e
Luiz Fernando <luizfernando_rodrigues12@hotmail.com>
2020-09-19 02:12:00 -0300

    Implementação dos modelos, implementado classe abstrata dos modelos e o modelo concreto Desenvolvedor

---
+++ src/Business/Models/Developer.cs
@@ -0,0 +1,19 @@
+﻿namespace Business.Models
+{
+    /// <summary>
+    /// Classe de modelo do desenvolvedor
+    /// </summary>
+    /// <remarks>
+    /// Autor   : Luiz Fernando
+    /// Data    : 19/09/2020
+    /// </remarks>
+    public class Developer : Entity
+    {
+        #region Properties
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Uf { get; set; }
+        #endregion Properties
+    }
+}
---
+++ src/Business/Models/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Business.Models
+{
+    /// <summary>
+    /// Classe abstrata base para os modelos, todos modelos deve herdar esta classe
+    /// </summary>
+    /// <remarks>
+    /// Autor   : Luiz Fernando
+    /// Data    : 19/09/2020
+    /// </remarks>
+    public abstract class Entity
+    {
+        #region Properties
+        public Guid Id { get; set; }
+        #endregion Properties
+
+        #region Cosntructor
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+        #endregion Constructor
+    }
+}
