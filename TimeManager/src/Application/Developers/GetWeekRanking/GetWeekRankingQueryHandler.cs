e51307e67999436d3d36c080fe403848d78913fd
Luiz de Paula <depauladev@gmail.com>
2020-09-21 02:53:58 -0300

    ranking

--- TimeManager/src/Application/Developers/GetWeekRanking/GetWeekRankingQueryHandler.cs
+++ TimeManager/src/Application/Developers/GetWeekRanking/GetWeekRankingQueryHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Dapper;
@@ -23,23 +24,27 @@ namespace TimeManager.Application.Developers.GetWeekRanking
             var connection = _sqlConnectionFactory.GetOpenConnection();
             var thisWeek = DateTime.Now.WeekNumberOfTheYear();
 
-            const string query = @"
-                SELECT TOP 5
-	                AVG(CalculatedHoursWorked) as AverageHoursWorked,
-	                D.Id as Developer_Id,
-	                D.Name as Developer_Name
-                FROM TimeReports TR
-                JOIN Developers D ON d.Id = TR.DeveloperId
-                GROUP BY 
-	                D.Id, D.Name, Tr.CalculatedWeekOfTheYear
-                HAVING
-	                CalculatedWeekOfTheYear = @Week
-                ORDER BY
-	                AverageHoursWorked DESC";
+            var data = await connection.QueryAsync<dynamic>(@"
+	            SELECT 
+		            SUM(TR.CalculatedHoursWorked) as SumWorkedWeekHOurs,
+		            MIN(TR.StartedAt) as FirstDayWorked,
+		            MAX(TR.EndedAt) as LastDayWorked,
+		            D.Id as 'Developer_Id',
+		            D.Name as 'Developer_Name'
+	            FROM TimeReports TR
+	            JOIN Developers D ON D.Id = TR.DeveloperId
+	            WHERE TR.CalculatedWeekOfTheYear = @Week
+	            GROUP BY 
+		            TR.CalculatedWeekOfTheYear, D.Id, D.Name
+            ", new { Week = thisWeek });
 
-            var data = await connection.QueryAsync<dynamic>(query, new { Week = thisWeek });
-
+            // @TODO
+            // Considerar que há dias que não foram trabalhados no meio da semana
             var realData = Slapper.AutoMapper.MapDynamic<RankingViewModel>(data);
+
+            // @TODO
+            // Ordenação e TOP 5 na propria query
+            realData.OrderByDescending(o => o.AverageHoursWorked);
 
             return new Response(realData);
         }
--- TimeManager/src/Application/Developers/GetWeekRanking/RankingViewModel.cs
+++ TimeManager/src/Application/Developers/GetWeekRanking/RankingViewModel.cs
@@ -1,8 +1,20 @@
-﻿namespace TimeManager.Application.Developers.GetWeekRanking
+﻿using System;
+
+namespace TimeManager.Application.Developers.GetWeekRanking
 {
     public class RankingViewModel
     {
         public DeveloperDto Developer { get; set; }
-        public double AverageHoursWorked { get; set; }
+        public double SumWorkedWeekHOurs { get; set; }
+        public DateTime FirstDayWorked { get; set; }
+        public DateTime LastDayWorked { get; set; }
+        public double AverageHoursWorked
+        {
+            get
+            {
+                var daysWorked = Math.Ceiling((LastDayWorked - FirstDayWorked).TotalDays);
+                return SumWorkedWeekHOurs /  daysWorked;
+            }
+        }
     }
 }
