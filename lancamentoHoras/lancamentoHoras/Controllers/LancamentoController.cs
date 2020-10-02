15357fd5a72fc2809ef15750dee145215a82cdcb
TiagoHasuda <tiago.ghasuda@gmail.com>
2020-10-02 15:53:49 -0300

    Initial commit

---
+++ lancamentoHoras/lancamentoHoras/Controllers/LancamentoController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lancamentoHoras.Database;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lancamentoHoras.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LancamentoController : ControllerBase
+    {
+        [HttpPost]
+        [Authorize]
+        public async Task<string> Post([FromForm] DateTime inicio, [FromForm] DateTime fim, [FromForm] int desenvolvedor)
+        {
+            return await DBQueries.NewHourEntry(inicio, fim, desenvolvedor);
+        }
+
+        [HttpGet]
+        [Route("ranking")]
+        public async Task<string> Get()
+        {
+            return await DBQueries.GetRanking();
+        }
+    }
+}
---
+++ lancamentoHoras/lancamentoHoras/Database/DBConnection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace lancamentoHoras.Database
+{
+    public static class DBConnection
+    {
+        private static readonly string connString = "server=localhost;user id=root;password=tiagocpenguin123;port=3310;database=lubydatabase;";
+
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connString);
+        }
+    }
+}
