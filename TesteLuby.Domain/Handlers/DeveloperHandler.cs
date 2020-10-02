d975f7017d943002cc8c2d6daa516cf77a562999
gustavoamorimqueiroz1 <66345851+gustavoamorimqueiroz1@users.noreply.github.com>
2020-10-02 14:07:50 -0300

    Implementação crud de desenvolvedor
    Implementação crud de desenvolvedor

--- TesteLuby.Domain/Handlers/DeveloperHandler.cs
+++ TesteLuby.Domain/Handlers/DeveloperHandler.cs
@@ -19,5 +19,138 @@ namespace TesteLuby.Domain.Handlers
             _authUser = authUser;
         }
 
+        #region Get Methods
+
+        public async Task<ICommandResult> GetByEmail(GetDeveloperByEmailCommand user)
+        {
+            try
+            {
+                string sql = $"Select * from users where users.email = '{user.Email}'";
+                var retorno = await Repository.GetOneBySql(sql);
+                ICommandResult resultadoServico = retorno == null
+                   ? new CommandResult((int)EStatus.NotFound, false, "Usuário não encontrado !", null)
+                   : new CommandResult((int)EStatus.Ok, true, "Usuário encontrado pelo e-mail!", retorno);
+                return resultadoServico;
+            }
+            catch (Exception ex)
+            {
+                throw new ArgumentException("Erro ao tentar buscar um usuário pelo e-mail. Erro: ", ex.Message);
+            }
+        }
+        #endregion
+
+        #region Posts Methods
+        public async Task<ICommandResult> CreateUser(CreateDeveloperCommand user)
+        {
+            try
+            {
+                var sqlExisteEmail = $"select * from users where email = '{user.Email}'";
+                var retornoExistenciaEmail = await Repository.GetOneBySql(sqlExisteEmail);
+                var sqlExisteUsuario = $"select * from users where username = '{user.UserName}'";
+                var retornoExistenciaUsuario = await Repository.GetOneBySql(sqlExisteUsuario);
+                if (retornoExistenciaEmail == null && retornoExistenciaUsuario == null)
+                {
+                    string sql = $"INSERT INTO users " +
+                        $"(username, email, password,phonenumber, lastacess) " +
+                        $"VALUES " +
+                        $"('{user.UserName}', '{user.Email}', '{user.Password}', '{user.PhoneNumber}', {DateTime.Now.Day}) ";
+                    var retorno = await Repository.InsertBySql(sql);
+                    ICommandResult resultadoServico = retorno == false
+                        ? new CommandResult((int)EStatus.InternalServerError, false, "Não foi possível adicionar um novo usuário !", null)
+                        : new CommandResult((int)EStatus.Created, true, "O usuário foi adicionado com sucesso !", retorno);
+                    return resultadoServico;
+                }
+                else
+                {
+                    return new CommandResult((int)EStatus.Forbidden, true, "Usuário ou email já cadastrado !", null);
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.StartsWith("23505"))
+                {
+                    return new CommandResult((int)EStatus.Forbidden, true, "Usuário já cadastrado !", null);
+                }
+                else
+                {
+                    return new CommandResult((int)EStatus.InternalServerError, false, "Erro Interno ao tentar adicionar um usuário. !", null);
+                }
+            }
+        }
+
+
+        public async Task<ICommandResult> CreateBusinessHour(CreateBusinessHourCommand businessHour)
+        {
+            try
+            {
+                    string sql = $"INSERT INTO businesshour " +
+                        $"(developerid, datetimestart, datetimeend) " +
+                        $"VALUES " +
+                        $"({businessHour.DeveloperId}, '{businessHour.DateTimeStart}', '{businessHour.DateTimeEnd}') ";
+                    var retorno = await Repository.InsertBySql(sql);
+                    ICommandResult resultadoServico = retorno == false
+                        ? new CommandResult((int)EStatus.InternalServerError, false, "Não foi possível lançar uma hora para o desenvolvedor!", null)
+                        : new CommandResult((int)EStatus.Created, true, "Hora lançada com sucesso!", retorno);
+                    return resultadoServico;
+            }
+            catch (Exception ex)
+            {
+                return new CommandResult((int)EStatus.InternalServerError, false, "Erro Interno ao tentar lançar hora!", null);
+            }
+        }
+
+        #endregion
+
+        #region Update Methods
+        public async Task<ICommandResult> UpdateDeveloper(UpdateDeveloperCommand update)
+        {
+            try
+            {
+                return await UpdateDeveloperResource(update);
+            }
+            catch
+            {
+                return new CommandResult((int)EStatus.InternalServerError, false,
+                   $"Erro ao tentar atualizar o usuário.", null);
+            }
+        }
+
+        internal async Task<ICommandResult> UpdateDeveloperResource(UpdateDeveloperCommand update)
+        {
+            try
+            {
+                string findUser = $"Select * from users where guid = '{update.Guid}' ";
+                var user = await Repository.GetOneBySql(findUser);
+                var sqlExisteEmail = $"select * from users where email = '{update.Email}' and users.guid != '{update.Guid}'";
+                var retornoExistenciaEmail = await Repository.GetOneBySql(sqlExisteEmail);
+                var sqlExisteUsuario = $"select * from users where username = '{update.UserName}' and users.guid != '{update.Guid}'";
+                var retornoExistenciaUsuario = await Repository.GetOneBySql(sqlExisteUsuario);
+
+                if (retornoExistenciaEmail == null)
+                {
+                    string sql =
+                        $"Update users set email = '{update.Email}', " +
+                        $"username = '{update.UserName}', firstname = '{update.FirstName}', " +
+                        $"lastname = '{update.LastName}', phonenumber = '{update.PhoneNumber}', rating = {update.Rating}  " +
+                        $"where guid = '{update.Guid}' ";
+                    var retorno = await Repository.UpdateBySql(sql);
+                    ICommandResult resultadoServico = retorno == false
+                       ? new CommandResult((int)EStatus.BadRequest, false, "Não foi possível atualizar o usuário! Usuário não encontrado.", null)
+                       : new CommandResult((int)EStatus.Ok, true, "Usuário atualizado com sucesso!", retorno);
+                    return resultadoServico;
+                }
+                else
+                {
+                    return new CommandResult((int)EStatus.BadRequest, false, "usuário ou email existentes.", null);
+                }
+            }
+            catch
+            {
+                return new CommandResult((int)EStatus.InternalServerError, false,
+                    $"Erro ao tentar atualizar o usuário.", null);
+            }
+        }
+        #endregion
+
     }
 }
--- TesteLuby.MainStartup/Controllers/DeveloperController.cs
+++ TesteLuby.MainStartup/Controllers/DeveloperController.cs
@@ -33,5 +33,110 @@ namespace TesteLuby.MainStartUp.Controllers
             _settings = settings ?? throw new ArgumentNullException(nameof(settings));
         }
 
+        /// <summary>
+        /// Busca desenvolvedor pelo e-mail
+        /// </summary>
+        /// <returns>Lista desenvolvedor pelo e-mail (JSON)</returns>
+        [HttpPost("email")]
+        public async Task<IActionResult> GetUserByEmail([FromBody]GetDeveloperByEmailCommand user)
+        {
+            var users = await _handler.GetByEmail(user);
+            if (users.Success)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                return StatusCode(500, users);
+            }
+        }
+
+        /// <summary>
+        /// Lançamento de hora
+        /// </summary>
+        [HttpPost("businessHour")]
+        [AllowAnonymous]
+        public async Task<IActionResult> PostBusinessHour(CreateBusinessHourCommand command)
+        {
+            var users= await _handler.CreateBusinessHour(command);
+            if (users.Success)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                switch (users.Status)
+                {
+                    case 400:
+                        return BadRequest(users);
+                    case 406:
+                        return BadRequest(users);
+                    case 500:
+                        return StatusCode(500, users);
+                    default:
+                        return BadRequest(users);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Adiciona um novo desenvolvedor
+        /// </summary>
+        [HttpPost("add")]
+        [AllowAnonymous]
+        public async Task<IActionResult> PostUser(CreateDeveloperCommand user)
+        {
+            user.Password = user.Password.Criptografa();
+            var users = await _handler.CreateUser(user);
+            if (users.Success)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                switch (users.Status)
+                {
+                    case 400:
+                        return BadRequest(users);
+                    case 406:
+                        return BadRequest(users);
+                    case 500:
+                        return StatusCode(500, users);
+                    default:
+                        return BadRequest(users);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Altera informações do desenvolvedor
+        /// </summary>
+        /// <param name="update"></param>
+        /// <returns>Altera informações do usuário</returns>
+        [HttpPut("update")]
+        [AllowAnonymous]
+        public async Task<IActionResult> UpdateUser(UpdateDeveloperCommand update)
+        {
+            var users = await _handler.UpdateDeveloper(update);
+            if (users.Success)
+            {
+                return Ok(users);
+            }
+            else
+            {
+                switch (users.Status)
+                {
+                    case 400:
+                        return BadRequest(users);
+                    case 406:
+                        return BadRequest(users);
+                    case 500:
+                        return StatusCode(500, users);
+                    default:
+                        return BadRequest(users);
+                }
+            }
+        }
+
     }
 }
