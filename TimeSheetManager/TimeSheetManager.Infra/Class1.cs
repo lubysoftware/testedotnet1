e5574b2bc252365f0269d752f1d2a12105c622fc
Vito <victor@selfsolucoes.com.br>
2020-10-19 18:28:07 -0300

    add Infrastructure.cs

--- TimeSheetManager/TimeSheetManager.Infra/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimeSheetManager.Infra
-{
-    public class Class1
-    {
-    }
-}
---
+++ TimeSheetManager/TimeSheetManager.Infra/Infrastructure.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TimeSheetManager.App.Handlers.DeveloperNS;
+using TimeSheetManager.App.Handlers.ProjectNS;
+using TimeSheetManager.Domain.Repositories;
+using TimeSheetManager.Infra.Context;
+using TimeSheetManager.Infra.Repositories;
+
+namespace TimeSheetManager.Infra {
+    public static class Infrastructure {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services) {
+            services.AddScoped<IDeveloperRepository, DeveloperRepository>();
+            setHandlers(services);
+
+            return services;
+        }
+
+        private static void setHandlers(IServiceCollection services) {
+            services.AddScoped<CreateDeveloperHandler>();
+            services.AddScoped<DeleteDeveloperHandler>();
+            services.AddScoped<UpdateDeveloperHandler>();
+            services.AddScoped<CreateProjectHandler>();
+            services.AddScoped<DeleteProjectHandler>();
+            services.AddScoped<UpdateProjectHandler>();
+        }
+    }
+}
