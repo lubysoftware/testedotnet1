bdca7ff943c86493bae27355e5ae3e0234c7b99c
Vito <victor@selfsolucoes.com.br>
2020-10-19 18:20:32 -0300

    fix Project repository

--- TimeSheetManager/TimeSheetManager.Domain/Repositories/IProjectRepository.cs
+++ TimeSheetManager/TimeSheetManager.Domain/Repositories/IProjectRepository.cs
@@ -5,10 +5,10 @@ using TimeSheetManager.Domain.Entities.Project;
 
 namespace TimeSheetManager.Domain.Repositories {
     public interface IProjectRepository {
-        Task Post(Project developer);
+        Task Post(Project Project);
         Task<Project> Get(Guid id);
         Task<List<Project>> GetAll();
-        Task Update(Project developer);
+        Task Update(Project Project);
         Task Delete(Guid id);
     }
 }
