8cfd6a98c201dcacdcdbd41453f96e3e373ae4b6
Vito <victor@selfsolucoes.com.br>
2020-10-20 09:49:57 -0300

    add Migrations

---
+++ TimeSheetManager/TimeSheetManager.Infra/Migrations/20201020124902_initialMigrate.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TimeSheetManager.Infra.Migrations
+{
+    public partial class initialMigrate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Developers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Developers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeSheet",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DevId = table.Column<Guid>(nullable: false),
+                    EntryTime = table.Column<DateTime>(nullable: false),
+                    ExitTime = table.Column<DateTime>(nullable: false),
+                    WeekId = table.Column<int>(nullable: false),
+                    TotalHours = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeSheet", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimeSheet_Developers_DevId",
+                        column: x => x.DevId,
+                        principalTable: "Developers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeSheet_DevId",
+                table: "TimeSheet",
+                column: "DevId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "TimeSheet");
+
+            migrationBuilder.DropTable(
+                name: "Developers");
+        }
+    }
+}
