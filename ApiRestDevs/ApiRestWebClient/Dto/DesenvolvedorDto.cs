4eea8d9c7dd3c6af901d744fc0031794395fe4b7
santisjp <santisjp@live.com>
2021-01-22 23:02:29 -0400

    Criando Web Application para consumir API de Lançamento de horas

---
+++ ApiRestDevs/ApiRestWebClient/Dto/DesenvolvedorDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiRestWebClient.Dto
+{
+    public class DesenvolvedorDto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
