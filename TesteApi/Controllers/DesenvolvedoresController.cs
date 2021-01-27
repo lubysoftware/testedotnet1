ff526bd8dd4a2ee2fead2103a55e0c7061711b08
victor27andre <victor27andre@gmail.com>
2021-01-26 22:07:26 -0300

    Starting the project and implementing the DesenvolvedoresController

---
+++ TesteApi/Controllers/DesenvolvedoresController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TesteApi.Models;
+
+namespace TesteApi.Controllers
+{
+    public class DesenvolvedoresController : ApiController
+    {
+        private static List<Desenvolvedor> desenvolvedores = new List<Desenvolvedor>();
+
+        public List<Desenvolvedor> Get()
+        {
+            return desenvolvedores;
+        }
+
+        public void Post(string nome)
+        {
+            if(!string.IsNullOrEmpty(nome))
+                desenvolvedores.Add(new Desenvolvedor(nome));
+        }
+
+        public void Delete(string nome)
+        {
+            desenvolvedores.RemoveAt(desenvolvedores.IndexOf(desenvolvedores.First(e => e.Nome.Equals(nome))));
+        }
+
+    }
+}
---
+++ TesteApi/Models/Desenvolvedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteApi.Models
+{
+    public class Desenvolvedor
+    {
+        public string Nome { get; set; }
+
+        public Desenvolvedor(string nome) // Construtor
+        {
+            this.Nome = nome;
+        }
+    }
+}
