4899f11ae76b1c942ef68166b9de1ffe1fe51a84
Carolline <carollinemarinho.nave@gmail.com>
2021-01-28 10:05:26 -0300

    Models

---
+++ testedotnet1/Models/Relogio_Ponto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testedotnet1.Models
+{
+    public class Relogio_Ponto
+    {
+        public int Id { get; set; }
+        public int Id_Desenvolvedor { get; set; }
+        public Desenvolvedor desenvolvedor { get; set; }
+        public DateTime entrada { get; set; }
+        public DateTime saida { get; set; }
+    }
+}
