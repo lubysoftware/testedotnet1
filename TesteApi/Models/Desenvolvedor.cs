f4ede6b67eb9b9953386b277d695d922eec509c3
victor27andre <victor27andre@gmail.com>
2021-01-28 23:13:11 -0300

    Adjusting the Models and doing migration

--- TesteApi/Models/Desenvolvedor.cs
+++ TesteApi/Models/Desenvolvedor.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+// HEROI
 namespace TesteApi.Models
 {
     public class Desenvolvedor
     {
+        public int Id { get; set; }
         public string Nome { get; set; }
+
+        public HoraTrabalhada HoraTrabalhada { get; set; }
 
         public Desenvolvedor(string nome) // Construtor
         {
---
+++ TesteApi/Models/Projeto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//ARMA
+namespace TesteApi.Models
+{
+    public class Projeto
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+        public Desenvolvedor Desenvolvedor { get; set; }
+
+        public int DesenvolvedorId { get; set; }
+    }
+}
