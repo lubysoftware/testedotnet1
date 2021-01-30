9a9d47482573e115afa1ee8a3da2a02a4706a33f
Carolline Marinho <carollinemarinho.nave@gmail.com>
2021-01-30 00:29:56 -0300

    Importando banco

---
+++ RelogioPonto/dbo/Tables/Projeto.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Projeto] (
+    [Id]   INT          IDENTITY (1, 1) NOT NULL,
+    [Nome] VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Projeto] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ RelogioPonto/dbo/Tables/Relogio_Ponto.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Relogio_Ponto] (
+    [Id]               INT      IDENTITY (1, 1) NOT NULL,
+    [Id_Desenvolvedor] INT      NOT NULL,
+    [entrada]          DATETIME NOT NULL,
+    [saida]            DATETIME NOT NULL,
+    CONSTRAINT [PK_Relogio_Ponto] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Relogio_Ponto_Relogio_Ponto] FOREIGN KEY ([Id_Desenvolvedor]) REFERENCES [dbo].[Desenvolvedor] ([Id]) NOT FOR REPLICATION
+);
+
+
+GO
+ALTER TABLE [dbo].[Relogio_Ponto] NOCHECK CONSTRAINT [FK_Relogio_Ponto_Relogio_Ponto];
+
