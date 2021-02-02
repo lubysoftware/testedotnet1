ce4f24543a71687d57f00fd123d2fb70e82a55c1
Fábio Pereira <fabiuhp@msn.com>
2021-02-01 23:23:30 -0300

    removed unused using namespaces

--- apiLuby/apiLuby/Controllers/DevelopersController.cs
+++ apiLuby/apiLuby/Controllers/DevelopersController.cs
@@ -1,12 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using apiLuby.Context;
-using Microsoft.EntityFrameworkCore;
+﻿using apiLuby.Context;
 using apiLuby.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
 
 namespace apiLuby.Controllers
 {
@@ -25,14 +22,21 @@ namespace apiLuby.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAllDevelopers()
         {
             var res = await developerContext.ToListAsync();
             return Ok(res);
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Developer>> GetDeveloper(Guid id)
+        {
+            var dev = await developerContext.FindAsync(id);
+            return Ok(dev);
+        }
+
         [HttpPost]
-        public async Task<IActionResult> Create([FromBody] Developer d)
+        public async Task<IActionResult> CreateDeveloper([FromBody] Developer d)
         {
             await developerContext.AddAsync(d);
             await context.SaveChangesAsync();
