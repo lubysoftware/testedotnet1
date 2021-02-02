2156999906737b2c151dd1e04dca4b318a2c4029
Victor André Costa da Silva <victorandre_@hotmail.com>
2021-02-02 11:04:53 -0300

    starting new project with PostgreSQL

---
+++ testeAPI/Models/Projeto.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TesteApi.Models
+{
+    public class Projeto
+    {
+        public int Id { get; set; }
+
+        public String Nome { get; set; }
+
+    }
+}
