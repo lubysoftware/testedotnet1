ac504e1057d80fff7de35d941f7fbb24cb877a47
Fábio Pereira <fabiuhp@msn.com>
2021-02-02 01:27:56 -0300

    controller finished

--- apiLuby/apiLuby/Controllers/DevelopersController.cs
+++ apiLuby/apiLuby/Controllers/DevelopersController.cs
@@ -36,9 +36,47 @@ namespace apiLuby.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateDeveloper([FromBody] Developer developer)
+        public async Task<IActionResult> PostDeveloper([FromBody] Developer developer)
         {
             await developerContext.AddAsync(developer);
+            await context.SaveChangesAsync();
+            return Ok(developer);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Developer>> PutDeveloper(Guid id, [FromBody]Developer developer)
+        {
+            if (developer.Id != id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(developer).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+                return Ok(developer);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Developer>> DeleteDeveloper(Guid id)
+        {
+            var developer = await developerContext.FindAsync(id);
+
+            if(developer == null)
+            {
+                return NotFound();
+            }
+
+            developerContext.Remove(developer);
             await context.SaveChangesAsync();
             return Ok();
         }
