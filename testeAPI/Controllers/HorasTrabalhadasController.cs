23333553ed3a68f5f4a92017203d72c40dfaeaf8
Victor André Costa da Silva <victorandre_@hotmail.com>
2021-02-04 13:20:22 -0300

    Return ranking of the 5 developers highest hours worked.

--- testeAPI/Controllers/HorasTrabalhadasController.cs
+++ testeAPI/Controllers/HorasTrabalhadasController.cs
@@ -4,9 +4,10 @@ using Microsoft.AspNetCore.Mvc;
 using TesteApi.Models;
 using TesteApi.Data;
 using Microsoft.EntityFrameworkCore;
+using TesteApi.VO;
 
 
-namespace TesteApi.Controllers
+ namespace TesteApi.Controllers
 {
     [Produces("application/json")]
     [Route("api/HorasTrabalhadas")]
@@ -42,26 +43,42 @@ namespace TesteApi.Controllers
             return NoContent();
         }
 
-        // GET: api/HorasTrabalhadas/top5
         [HttpGet, Route("top5")]
-        public IEnumerable<HoraTrabalhada> getTop5()
+        public IEnumerable<HoraTrabalhadaVO> top5()
         {
+            var result = (from totalHoras in _context.HorasTrabalhadas select new HoraTrabalhada
+            {
+                Id = totalHoras.Id,
+                DesenvolvedorId = totalHoras.DesenvolvedorId,
+                ProjetoId = totalHoras.ProjetoId,
+                DtInicio = totalHoras.DtInicio,
+                DtFim = totalHoras.DtFim,
+                Desenvolvedor = (from desenvolvedor in _context.Desenvolvedores 
+                                where desenvolvedor.Id == totalHoras.DesenvolvedorId 
+                                select new Desenvolvedor{
+                                    Id = desenvolvedor.Id,
+                                    Nome = desenvolvedor.Nome
+                                }).FirstOrDefault(),
+                Projeto = (from projeto in _context.Projetos
+                            where projeto.Id == totalHoras.ProjetoId
+                            select new Projeto{
+                                Id = projeto.Id,
+                                Nome= projeto.Nome
+                            }).FirstOrDefault()
 
-            var top5 = _context.HorasTrabalhadas
-                        .Include(e => e.Projeto)
-                        .Include(e => e.Desenvolvedor)
-                        .ToList();
 
-            return top5;
+            }).ToList().GroupBy(e => new { e.Desenvolvedor.Nome, e.DesenvolvedorId} )
+            .Select( g => new HoraTrabalhadaVO 
+            {
+                DesenvolvedorId = g.Key.DesenvolvedorId,
+                NomeDesenvolvedor = g.Key.Nome,
+                TotalHoras = g.Sum(e => e.DtFim.Subtract(e.DtInicio).TotalHours)
+            }).OrderByDescending(e => e.TotalHoras)
+            .Take(5)
+            .ToList();
+
+            return result;
         }
-
-        /* NOTETIONS!
-        Take(N) - O operador Take é usado para selecionar os primeiros N objetos de uma coleção.
-        Skip(N) - O operador Skip ignora o(s) primeiro(s) N objetos de uma coleção.
-        */
-        
-
-
     }
 
 }
